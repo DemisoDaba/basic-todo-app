c6fbb584da6ceac72a4a98e57888443aca9cd4aa
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-10-31 12:39:42 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -16,4 +16,18 @@ def list_tasks():
 # Create a New Task
 @app.route('/tasks/new', methods=['GET', 'POST'])
 def create_task():
-    # ... (your other route definitions)
+    if request.method == 'POST':
+        # Process the form data and create a new task
+        title = request.form['title']
+        description = request.form['description']
+        # Handle due date and other task attributes
+
+        # Example: Create a task in the database (if using a database)
+        new_task = Task(title=title, description=description)
+        db.session.add(new_task)
+        db.session.commit()
+
+        flash('Task created successfully!', 'success')
+        return redirect('/tasks')
+
+    return render_template('create_task.html')
