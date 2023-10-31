f2f77710fdf712bab959430f0027fac2a4fc7cfe
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-10-31 13:21:20 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
+# Import necessary modules
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+
+# Configure the SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # SQLite database file in your project folder
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking
+
+# Create an instance of the SQLAlchemy class
 db = SQLAlchemy(app)
-
-from app import routes
-
