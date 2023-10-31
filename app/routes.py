5c9c5024037807e31e3f5c4155a2e4359e42e194
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-10-31 13:18:39 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,7 @@
 from app import app, db
-from app.models import Task
-from flask import render_template, request, redirect, flash
+from flask import render_template
 
-# Root Route (Index Page)
+# Define your routes here
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# List Tasks and Create New Task
-@app.route('/tasks', methods=['GET', 'POST'])
-def tasks():
-    if request.method == 'POST':
-        # Process the form data and create a new task
-        title = request.form['title']
-        description = request.form['description']
-        # Example: Create a task in the database (if using a database)
-        new_task = Task(title=title, description=description)
-        db.session.add(new_task)
-        db.session.commit()
-        flash('Task created successfully!', 'success')
-        return redirect('/tasks')
-
-    tasks = Task.query.all()  # Retrieve tasks from the database (if using a database)
-    return render_template('task_list.html', tasks=tasks)
-
