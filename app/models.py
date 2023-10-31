345f09a2c2f5c980528e7026acd17b91d1cf5126
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-10-31 13:27:47 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,11 +2,7 @@ from app import db
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), index=True)
-    description = db.Column(db.String(2000))
-    due_date = db.Column(db.DateTime)
-    completed = db.Column(db.Boolean, default=False)
+    title = db.Column(db.String(200), nullable=False)
 
-    def __repr__(self):
-        return f'<Task {self.title}>'
-
+    def __init__(self, title):
+        self.title = title
