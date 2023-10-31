768644ff7dbbfe12f3a1e53273f7cd4b9677f747
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-10-31 12:45:16 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,33 @@
+from app import app, db
+from app.models import Task
+from flask import render_template, request, redirect, flash
+
+# Root Route (Index Page)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# List Tasks
+@app.route('/tasks', methods=['GET'])
+def list_tasks():
+    tasks = Task.query.all()  # Retrieve tasks from the database (if using a database)
+    return render_template('task_list.html', tasks=tasks)
+
 # Create a New Task
 @app.route('/tasks/new', methods=['GET', 'POST'])
 def create_task():
-if request.method == 'POST':
-    # Process the form data and create a new task
-    title = request.form['title']
-    description = request.form['description']
-    # Handle due date and other task attributes
+    if request.method == 'POST':
+        # Process the form data and create a new task
+        title = request.form['title']
+        description = request.form['description']
+        # Handle due date and other task attributes
 
-    # Example: Create a task in the database (if using a database)
-    new_task = Task(title=title, description=description)
-    db.session.add(new_task)
-    db.session.commit()
+        # Example: Create a task in the database (if using a database)
+        new_task = Task(title=title, description=description)
+        db.session.add(new_task)
+        db.session.commit()
 
-    flash('Task created successfully!', 'success')
-    return redirect('/tasks')
+        flash('Task created successfully!', 'success')
+        return redirect('/tasks')
 
-return render_template('create_task.html')
+    return render_template('create_task.html')
