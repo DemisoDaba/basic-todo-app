5cb74ed93c688680007d5df52f8f8caa3c23e528
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-10-31 13:16:09 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Configure the database URI (replace 'sqlite:///your-database-name.db' with your actual database URI)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your-database-name.db'
+# Make sure that Flask knows where to find your templates folder.
+app.template_folder = '../templates'
 
-# Initialize the SQLAlchemy extension
-db = SQLAlchemy(app)
-
-# Additional configurations (e.g., secret key, session configuration) can be added here.
-
-# Import the routes module to include route handlers
 from app import routes
-
