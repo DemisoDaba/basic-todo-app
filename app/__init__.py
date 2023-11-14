df97e461563f80018e3ff275f9afcd29a4b226ff
Demisodaba <demo.nkmt1@gmail.com>
2023-11-14 23:28:20 +0300

    My App

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-db = SQLAlchemy(app)
-
-from app import routes
--- app/models.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-
-    def __init__(self, title):
-        self.title = title
