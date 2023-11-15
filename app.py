79642cc5282c344b510715bbe81390ce73796d7d
Demisodaba <demo.nkmt1@gmail.com>
2023-11-15 11:10:00 +0300

    Updated

--- app.py
+++ app.py
@@ -2,12 +2,15 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_app().run(debug=True)
 
